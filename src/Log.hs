3193004699cf92ab22828da009df50396f617a70
Zesen Qian <git@riaqn.org>
2016-12-28 16:52:05 +0800

    control log level by env var

--- src/Log.hs
+++ src/Log.hs
@@ -7,9 +7,19 @@ import System.Log.Formatter
 
 import System.IO
 
+import System.Environment
+import Text.Read
+
+import Control.Monad.Trans.Maybe
+
 setup :: IO ()
 setup = do
-  h <- streamHandler stderr DEBUG >>= \lh -> return $ setFormatter lh $
+
+  m <- runMaybeT $ do
+    x <- MaybeT $ lookupEnv "LOG"
+    MaybeT $ return $ readMaybe x
+  let l = maybe INFO (id) m
+  h <- streamHandler stderr l >>= \lh -> return $ setFormatter lh $
     simpleLogFormatter "[$tid : $loggername] $msg"
     
-  updateGlobalLogger rootLoggerName ((setLevel INFO) . setHandlers [h])
+  updateGlobalLogger rootLoggerName ((setLevel l) . setHandlers [h])
